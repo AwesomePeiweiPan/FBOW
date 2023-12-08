da87171b3761c74d2a851d14be0440e74b8415a9
Peiwei Pan <913746703ppw@gmail.com>
2023-12-08 12:18:45 +0100

    VOC Create version 0

---
+++ VocCreate/fbow_create_voc_step0.cpp
@@ -0,0 +1,172 @@
+//First step of creating a vocabulary is extracting features from a set of images. We save them to a file for next step
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "fbow.h"
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#ifdef USE_CONTRIB
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#endif
+#include "dirreader.h"
+using namespace fbow;
+using namespace std;
+
+
+//command line parser
+class CmdLineParser{int argc; char **argv; public: CmdLineParser(int _argc,char **_argv):argc(_argc),argv(_argv){}  bool operator[] ( string param ) {int idx=-1;  for ( int i=0; i<argc && idx==-1; i++ ) if ( string ( argv[i] ) ==param ) idx=i;    return ( idx!=-1 ) ;    } string operator()(string param,string defvalue="-1"){int idx=-1;    for ( int i=0; i<argc && idx==-1; i++ ) if ( string ( argv[i] ) ==param ) idx=i; if ( idx==-1 ) return defvalue;   else  return ( argv[  idx+1] ); }};
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+void wait()
+{
+    cout << endl << "Press enter to continue" << endl;
+    getchar();
+}
+
+
+
+vector< cv::Mat  >  loadFeatures( std::vector<string> path_to_images,string descriptor="")  {
+    //select detector
+    cv::Ptr<cv::Feature2D> fdetector;
+    if (descriptor=="orb")        fdetector=cv::ORB::create(2000);
+    else if (descriptor=="brisk") fdetector=cv::BRISK::create();
+#ifdef OPENCV_VERSION_3
+    else if (descriptor=="akaze") fdetector=cv::AKAZE::create(cv::AKAZE::DESCRIPTOR_MLDB,  0,  3, 1e-4);
+#endif
+#ifdef USE_CONTRIB
+    else if(descriptor=="surf" )  fdetector=cv::xfeatures2d::SURF::create(15, 4, 2);
+#endif
+
+    else throw std::runtime_error("Invalid descriptor");
+    assert(!descriptor.empty());
+    vector<cv::Mat>    features;
+
+
+    cout << "Extracting   features..." << endl;
+    for(size_t i = 0; i < path_to_images.size(); ++i)
+    {
+        vector<cv::KeyPoint> keypoints;
+        cv::Mat descriptors;
+        cout<<"reading image: "<<path_to_images[i]<<endl;
+        cv::Mat image = cv::imread(path_to_images[i], 0);
+        if(image.empty()) {
+            std::cerr<<"Could not open image:"<<path_to_images[i]<<std::endl;
+            continue;
+        }
+        fdetector->detectAndCompute(image, cv::Mat(), keypoints, descriptors);
+        cout<<"extracting features: total= "<<keypoints.size() <<endl;
+        features.push_back(descriptors);
+        cout<<"done detecting features"<<endl;
+    }
+    return features;
+}
+
+// ----------------------------------------------------------------------------
+void saveToFile(string filename,const vector<cv::Mat> &features,  std::string  desc_name,bool rewrite =true){
+
+    //test it is not created
+    if (!rewrite){
+        std::fstream ifile(filename, std::ios::binary);
+        if (ifile.is_open())//read size and rewrite
+            std::runtime_error( "ERROR::: Output File "+filename+" already exists!!!!!" );
+    }
+	std::ofstream ofile(filename, std::ios::binary);
+    if (!ofile.is_open()){cerr<<"could not open output file"<<endl;exit(0);}
+
+    char _desc_name[20];
+    desc_name.resize(min(size_t(19),desc_name.size()));
+    strcpy(_desc_name,desc_name.c_str());
+    ofile.write(_desc_name,20);
+
+    uint32_t size=features.size();
+    ofile.write((char*)&size,sizeof(size));
+    for(auto &f:features){
+        if( !f.isContinuous()){
+            cerr<<"Matrices should be continuous"<<endl;exit(0);
+        }
+        uint32_t aux=f.cols; ofile.write( (char*)&aux,sizeof(aux));
+        aux=f.rows; ofile.write( (char*)&aux,sizeof(aux));
+        aux=f.type(); ofile.write( (char*)&aux,sizeof(aux));
+        ofile.write( (char*)f.ptr<uchar>(0),f.total()*f.elemSize());
+    }
+}
+
+bool isImageFile(const string& fileName) {
+    const vector<string> imageExtensions = {".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".gif"};
+    for (const auto& ext : imageExtensions) {
+        if (fileName.rfind(ext) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+vector<string> readImagePathsFromFolder(const string& folder_path) {
+    vector<string> imagePaths;
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(folder_path.c_str())) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            string file_name = string(ent->d_name);
+            if (isImageFile(file_name)) {
+                imagePaths.push_back(folder_path + "/" + file_name);
+            }
+        }
+        closedir(dir);
+    }
+    return imagePaths;
+}
+
+vector<string> readFolderPaths(const string& file_path) {
+    vector<string> folderPaths;
+    ifstream file(file_path);
+    string line;
+    while (getline(file, line)) {
+        folderPaths.push_back(line);
+    }
+    return folderPaths;
+}
+
+// ----------------------------------------------------------------------------
+//两段程序都是关于从图像中提取特征并将其保存到文件的。第二段代码中的一些修改和改进似乎是为了增强程序的鲁棒性、提供更清晰的输出信息，并简化从目录中读取图像的过程。
+int main(int argc, char **argv) {
+    try {
+        if (argc < 4) {
+            cerr << "Usage:  descriptor_name output path_to_folders_file.txt" << endl;
+            return -1;
+        }
+
+        string descriptor = argv[1];
+        string output = argv[2];
+        string folders_file = argv[3];
+
+        vector<string> folders = readFolderPaths(folders_file);
+
+        vector<cv::Mat> features;
+        for (const string& folder : folders) {
+            auto images = readImagePathsFromFolder(folder);
+            auto folderFeatures = loadFeatures(images, descriptor);
+            features.insert(features.end(), folderFeatures.begin(), folderFeatures.end());
+            // 移除rows为0的元素
+            features.erase(std::remove_if(features.begin(), features.end(), 
+                         [](const cv::Mat &f) { return f.rows == 0; }), 
+                         features.end());
+        }
+
+        // 保存特征到文件
+        std::cerr << "Saving to " << output << std::endl;
+        saveToFile(output, features, descriptor);
+
+    } catch (std::exception &ex) {
+        cerr << ex.what() << endl;
+    }
+
+    return 0;
+}
